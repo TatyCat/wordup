25e60b13f0849b7f8cbcd4d4120aac3cfa1e0673
TatyCat <williams.tatyanna@gmail.com>
2019-05-31 16:58:59 -0400

    Added beginning animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    loading: false,
+  }
+
+  showLoader = () => {
+    this.setState({ loading: true, })
+  }
+
   render() {
     return (
       <>
         <main className="form-container">
-          <h1>WordUp</h1>
-        </main>
+          <h1 onClick={this.showLoader} className={this.state.loading ? "textUp" : ""}>WordUp</h1>
+          <div className={this.state.loading ? "spinner" : ""}>
+            <div className="rect1"></div>
+            <div className="rect2"></div>
+            <div className="rect3"></div>
+            <div className="rect4"></div>
+            <div className="rect5"></div>
+          </div>
 
+        </main>
       </>)
   }
 }
